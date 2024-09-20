eb11eab3f2e565ac8c3a1a045bd47b71db368ce6
Dmitrii <d.n.baklai@gmail.com>
2024-09-20 20:46:38 +0300

    fix

--- src/pages/Sidebar.vue
+++ src/pages/Sidebar.vue
@@ -49,7 +49,10 @@ onMounted(async () => {
       <div style="display: flex; align-items: center">
         <img src="@/assets/logo.svg" alt="logo" class="mr-2" height="24" width="24" />
 
-        <h1 class="text-2xl font-semibold text-gray-800 capitalize dark:text-white lg:text-3xl">
+        <h1
+          class="text-2xl font-semibold text-orange-500 capitalize dark:text-white lg:text-3xl"
+          style="-webkit-text-stroke: 1px gray"
+        >
           GPT Asst for Telegram
         </h1>
       </div>
@@ -123,34 +126,40 @@ onMounted(async () => {
     </div>
 
     <div class="flex flex-col justify-center w-full p-8 pt-0 lg:w-1/2 lg:px-12 xl:px-24">
-      <div class="-mx-2 md:items-center md:flex">
+      <div class="-mx-2 md:items-center md:flex" v-if="!apikey">
         <div class="flex-1 px-2 mt-4 md:mt-0">
-          <label for="apikey" class="block mb-2 text-sm text-gray-600 dark:text-gray-200"
-            >OpenAI API</label
-          >
+          <label for="apikey" class="block mb-2 text-sm font-bold text-gray-600 dark:text-gray-200">
+            OpenAI API
+          </label>
           <input
             id="apikey"
             type="password"
             v-model="apikey"
             placeholder="Enter your OpenAI API key"
-            v-if="!apikey"
             class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
           />
-
-          <button
-            @click="onResetKey"
-            :disabled="!apikey"
-            class="w-full px-6 py-3 mt-4 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
-          >
-            Reset OpenAI API Key
-          </button>
+          <p class="mt-3 text-xs text-gray-400 dark:text-gray-600">
+            Input OpenAI API key you're ready to make your first API request.
+          </p>
         </div>
       </div>
 
-      <div class="w-full mt-4">
-        <label for="yourself" class="block mb-2 text-sm text-gray-600 dark:text-gray-200"
-          >About yourself</label
+      <div class="md:items-center md:flex" :hidden="!apikey">
+        <button
+          @click="onResetKey"
+          class="w-full px-6 py-3 mt-4 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-orange-500 rounded-md hover:bg-orange-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
         >
+          Reset OpenAI API Key
+        </button>
+        <p class="mt-3 text-xs text-gray-400 dark:text-gray-600">
+          Input OpenAI API key you're ready to make your first API request.
+        </p>
+      </div>
+
+      <div class="w-full mt-4">
+        <label for="yourself" class="block mb-2 text-sm font-bold text-gray-600 dark:text-gray-200">
+          About yourself
+        </label>
         <textarea
           id="yourself"
           rows="5"
@@ -158,6 +167,18 @@ onMounted(async () => {
           placeholder="Enter information about yourself"
           class="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-56 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
         ></textarea>
+        <p class="mt-3 text-xs text-gray-400 dark:text-gray-600">
+          This field is used to set the initial context or set specific instructions for the model.
+        </p>
+      </div>
+
+      <div class="md:items-center md:flex" :hidden="!apikey">
+        <button
+          @click="onResetKey"
+          class="w-full px-6 py-3 mt-4 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+        >
+          Open Telegram Web
+        </button>
       </div>
     </div>
   </section>
