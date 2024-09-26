e516b55255c7ae671894da80b246937c40073421
Dmitrii <d.n.baklai@gmail.com>
2024-09-26 19:04:04 +0300

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -37,11 +37,11 @@ onMounted(async () => {
       {{ item.title }}
     </a>
 
-    <div class="w-full">
-      <details class="group py-8">
+    <div class="flex flex-col gap-y-6 w-full">
+      <details class="group py-2">
         <summary class="flex cursor-pointer items-center text-gray-500 dark:text-gray-400">
           <span class="h-px flex-1 bg-gray-300"></span>
-          <span class="shrink-0 px-6">The personality described</span>
+          <span class="shrink-0 px-6 uppercase">The personality described</span>
           <span class="h-px flex-1 bg-gray-300"></span>
         </summary>
 
@@ -54,19 +54,26 @@ onMounted(async () => {
         <button
           type="button"
           @click="$router.push({ name: 'options' })"
-          class="w-full px-6 py-3 text-sm font-medium text-center tracking-wide text-white transition-colors duration-300 transform bg-primary-500 rounded-md hover:bg-primary-400 focus:outline-none focus:ring focus:ring-primary-300 focus:ring-opacity-50"
+          class="w-full my-4 px-6 py-3 text-sm font-medium text-center tracking-wide text-white transition-colors duration-300 transform bg-primary-500 rounded-md hover:bg-primary-400 focus:outline-none focus:ring focus:ring-primary-300 focus:ring-opacity-50"
         >
           Open options
         </button>
       </details>
-    </div>
 
-    <button
-      type="button"
-      @click="onResetHandler"
-      class="group mt-10 flex w-full items-center justify-center gap-2 rounded-md bg-orange-500 hover:bg-orange-600 shadow-sm px-5 py-3 text-white transition focus:outline-none"
-    >
-      <span class="text-sm font-medium"> Reset API Key </span>
-    </button>
+      <details class="group py-2">
+        <summary class="flex cursor-pointer items-center text-gray-500 dark:text-gray-400">
+          <span class="h-px flex-1 bg-gray-300"></span>
+          <span class="shrink-0 px-6 uppercase">GPT API KEY</span>
+          <span class="h-px flex-1 bg-gray-300"></span>
+        </summary>
+        <button
+          type="button"
+          @click="onResetHandler"
+          class="group my-4 flex w-full items-center justify-center gap-2 rounded-md bg-orange-500 hover:bg-orange-600 shadow-sm px-5 py-3 text-white transition focus:outline-none"
+        >
+          <span class="text-sm font-medium"> Reset API Key </span>
+        </button>
+      </details>
+    </div>
   </div>
 </template>
